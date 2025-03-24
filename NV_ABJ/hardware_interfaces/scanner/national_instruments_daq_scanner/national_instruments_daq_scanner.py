9c9b93aba3f161cbefb4d401ba6710496d029766
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2025-03-24 15:02:43 -0700

    cleaned up scanner and added conversion factors. Tested with a multimeter all functions appear to work as intended

--- NV_ABJ/hardware_interfaces/scanner/national_instruments_daq_scanner/national_instruments_daq_scanner.py
+++ NV_ABJ/hardware_interfaces/scanner/national_instruments_daq_scanner/national_instruments_daq_scanner.py
@@ -11,18 +11,18 @@ from dataclasses import dataclass
 
 # National instruments imports 
 import nidaqmx
-``
 
 from NV_ABJ import ScannerSingleAxis
 
 @dataclass
 class NationalInstrumentsDaqSingleAxisScannerConfiguration:
     """
-    device_name_input:str = Device name that the voltage input is attached to e.g. "PXI1Slot3"
-    device_name_output:str = Device name for the out put
-    channel_name_input:str = Name for the channel input e.g. "ai0"
-    channel_name_output:str = Name for the channel output e.g. "ao0"
     conversion_volts_per_meter:float = How many volts would it be to move the scanner one meter yes this is large but thats just for conversion to standard units 
+
+    device_name_output:str = Device name for the output e.g. "PXI1Slot3"
+    channel_name_output:str = Name for the channel output e.g. "ao0"
+    device_name_input:str = Device name that the voltage input is attached to e.g. "PXI1Slot3" default None
+    channel_name_input:str = Name for the channel input e.g. "ai0" default None
 
     If you want to set limits to protect the scanner 
     position_limits_m:tuple = (Lower limit on distance, Upper limit on distance) in meters. This is to stop over-volting or going past a arbitrary limit. default (None,None) 
@@ -34,20 +34,24 @@ class NationalInstrumentsDaqSingleAxisScannerConfiguration:
     samples_per_read:int=100
     
     """
-    device_name_input:str
+    conversion_volts_per_meter_setting:float
+
     device_name_output:str
-    channel_name_input:str
     channel_name_output:str
-    conversion_volts_per_meter:float
+    device_name_input:str = None
+    channel_name_input:str = None
 
     # Limits on the device if we want to set them
     position_limits_m:tuple = (None,None) # This is based on the scanner if it has a limit of 3um or a voltage limit it will throw an error if it is exceeded 
+    conversion_volts_per_meter_getting:float = None # If the reading voltage is different to the setting voltage conversion (set to None results in the conversions being equal)
 
     #This is the configuration for the stabilization of the scanner 
     timeout_waiting_for_voltage_set_s:int = 10
     stability_voltage_difference:float = 0.004
     voltage_sample_rate:int=1000
     samples_per_read:int=100
+
+    _position_m:float = None # Where the device is set to for tracking purposes only
 
 
 
@@ -62,11 +66,24 @@ class NationalInstrumentsDaqSingleAxisScanner(ScannerSingleAxis):
     #########################################################################################################################################################################    
     def set_position_m(self, position):
         voltage = self.position_to_voltage(position)
-        self.wait_for_voltage(voltage)
-    
+
+        # Checks to see if the scanner controlled by the daq is monitored by a port or not (attocube positioner versus fsm functionality)
+        if self._device_configuration.channel_name_input != None:
+            self.wait_for_voltage(voltage)
+        else:
+            self.voltage_out(voltage)
+            self._device_configuration._position_m = position
+        
     def get_position_m(self):
-        voltage = self.read_voltage()
-        return voltage/self._device_configuration.conversion_volts_per_meter
+        if self._device_configuration.channel_name_input != None:
+            voltage = self.read_voltage()
+            if self._device_configuration.conversion_volts_per_meter_getting == None:
+                return voltage/self._device_configuration.conversion_volts_per_meter_setting
+            else:
+                return voltage/self._device_configuration.conversion_volts_per_meter_getting 
+
+        else:
+            return self._device_configuration._position_m
     
     # This is handled by the daq
     def make_connection(self):
@@ -96,7 +113,7 @@ class NationalInstrumentsDaqSingleAxisScanner(ScannerSingleAxis):
                 raise ValueError(f"Can not enter a value larger than upper position limit of {position_limits_m[1]}")
         
         # This will set the voltage if no errors are returned 
-        voltage_set = self._device_configuration.conversion_volts_per_meter*position
+        voltage_set = self._device_configuration.conversion_volts_per_meter_setting*position
         
         return voltage_set
 
@@ -127,14 +144,18 @@ class NationalInstrumentsDaqSingleAxisScanner(ScannerSingleAxis):
 
     
     def wait_for_voltage(self,voltage):
+        channel_address_in = self._device_configuration.device_name_input+"/"+self._device_configuration.channel_name_input
+
         try:
             self.voltage_out(voltage)
             timeout_start = time.time()
 
+            # This doesn't actually use the getting value to determine distance it simply waits for the voltage to settle 
+
             try:
 
                 with nidaqmx.Task() as task:
-                    task.ai_channels.add_ai_voltage_chan(self.daq_class_input.id_daq_channel, terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
+                    task.ai_channels.add_ai_voltage_chan(channel_address_in, terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
                     task.timing.cfg_samp_clk_timing(rate=self._device_configuration.voltage_sample_rate,
                                                      sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS, samps_per_chan=self._device_configuration.samples_per_read)
 
@@ -158,5 +179,5 @@ class NationalInstrumentsDaqSingleAxisScanner(ScannerSingleAxis):
                 raise Exception(f"DAQmx error occurred: {e}")
                
         except:
-            raise Exception("Failed to stabalize voltage output")
+            raise Exception("Failed to stabilize voltage output")
     
