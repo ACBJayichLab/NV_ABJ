46edc2d3d227efbcc1e20f048855a04294ef45ba
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2025-03-26 12:37:44 -0700

    can connect to sumitomo

---
+++ NV_ABJ/hardware_interfaces/cryogenic_compressors/sumitomo_FA-40/sumitomo_fa_40.py
@@ -0,0 +1,8 @@
+from sumitomo_f70 import SumitomoF70
+
+with SumitomoF70(com_port='COM4') as f70:
+    # Insert commands here (full list in docs)
+    # For example:
+    t1, t2, t3, t4 = f70.read_all_temperatures()
+
+print(t1,t2,t3,t4)
