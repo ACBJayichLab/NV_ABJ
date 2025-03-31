a38b0531963765c729b73f0f6633e260f7e50b7c
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2025-03-31 00:08:29 -0700

    linear pulse generation beta is done for pulse blaster through the command line

--- NV_ABJ/hardware_interfaces/sequence_generation/spbicl_pulse_blaster/SpbiclPulseBlaster.py
+++ NV_ABJ/hardware_interfaces/sequence_generation/spbicl_pulse_blaster/SpbiclPulseBlaster.py
@@ -2,10 +2,10 @@ import subprocess
 from tempfile import TemporaryDirectory
 from os.path import join
 
-from NV_ABJ import PulseGenerator
+from NV_ABJ import PulseGenerator, Sequence,seconds
 
 class SpbiclPulseBlaster(PulseGenerator):
-    def __init__(self,clock_frequency_megahertz:int=500, maximum_step_time_s:float = 6.9):
+    def __init__(self,clock_frequency_megahertz:int=500, maximum_step_time_s:float = 6,available_ports:int=23):
         """This class interfaces with the pulse blaster using the command line interpreter provided by 
         SpinCore as an exe "spbicl.exe" 
 
@@ -13,10 +13,13 @@ class SpbiclPulseBlaster(PulseGenerator):
             devices (list): list of sequence controlled devices 
             clock_frequency_megahertz (float, optional): What the pulse blaster will be set to. Defaults to 500.
             maximum_step_time_s (float, optional): This is the maximum time a step can take if it is longer it will be broken into n steps 
+            available_ports (int, optional): How many bits the pulse blaster can control. Defaults to 23
         """
         self.clock_frequency_megahertz = clock_frequency_megahertz
         self.maximum_step_time_s = maximum_step_time_s
+        self.available_ports = available_ports
         self._locked_commands = False
+
     
     def load_sequence(self, sequence:str)->int:
         """
@@ -27,9 +30,9 @@ class SpbiclPulseBlaster(PulseGenerator):
             sequence(str): string formatted in the style of a sequence file. Loading a empty string will clear the pulse blaster
 
         example of sequence:
-
+        
             label: 0b1111 1111 1111 1111 1111 1111, 500 ms
-                0b0000 0000 0000 0000 0000 0000, 500 ms, branch, label
+                   0b0000 0000 0000 0000 0000 0000, 500 ms, branch, label
 
         Returns:
             int: This is zero that indicates correct loading and -1 if it failed to load to the device other errors may have different numbers
@@ -68,7 +71,10 @@ class SpbiclPulseBlaster(PulseGenerator):
         # Prevents updating devices until stopped 
         self._locked_commands = True
         
-        return response
+        if response != 0:
+            raise Exception("Failed to start program to pulse blaster")
+        else:
+            return response
     
     def stop(self)->int:
         """
@@ -86,7 +92,10 @@ class SpbiclPulseBlaster(PulseGenerator):
         # Unlocks commands for other items 
         self._locked_commands = False
         
-        return response
+        if response != 0:
+            raise Exception("Failed to stop program to pulse blaster")
+        else:
+            return response
     
     def clear(self)->int:
         """
@@ -100,41 +109,121 @@ class SpbiclPulseBlaster(PulseGenerator):
 
         if not self._locked_commands:
             response = self.load_sequence(sequence=empty_sequence)
-            return response
+            return response   
+        
         else:
             raise Warning("To clear sequence you must stop the sequence")
 
     def update_devices(self, devices:list)->int:
+        """Takes a list of sequence controlled devices and creates a sequence loads it into the pulse blaster and starts it 
+
+        Args:
+            devices (list): This is a list of sequence controlled devices 
+
+        Raises:
+            Warning: If the pulse blaster is running it will not let you load a sequence this is to prevent errors where a sequence is stopped during a 
+            measurement 
+
+        Returns:
+            int: _description_
+        """
         if not self._locked_commands:
-            print(devices)
+            seq = Sequence()
+            seq.add_step(devices=devices)
+            seq_text = self.generate_sequence(seq)
+            self.load(sequence=seq_text)
+            response = self.start()
+            return response
+
         else:
             raise Warning("To update devices you must stop the sequence")
+
     
-    def generate_sequence(self,sequence_class)->str:
+    def generate_sequence(self,sequence_class:Sequence)->str:
         """This function takes the sequence class and converts it into a format that can be interpreted by the pulse blaster 
 
         Args:
-            sequence_class (_type_): _description_
+            sequence_class (Sequence): A sequence of the devices and times you wish to add
 
         Returns:
             str: This returns a string that can be used to load the sequence into the pulse blaster 
             This command is going to commonly be followed by load(sequence) they are kept separate because load 
             is a more general function and you may want to generate sequences and save them without 
         """
-        ...
-    
 
-    
+        def adding_lines(ind:int,addresses:int,duration:int,seq_length:int,seq_text:str)->str:
+            """Used locally to add lines to the sequence 
+            Args:
+                ind (int): what iteration of the loops we are on/ what line on the string we are writing 
+                addresses (int): what binary form of addresses are on
+                duration (int): how long in ns it is for
+                seq_length (int): how long the overall string we are writing is
+                seq_text (str): the string we are writing 
 
-if __name__ == "__main__":
+            Returns:
+                str: the sequence text string with the appropriate line added 
+
+            Uses a continuous looping method and (branch to start) and uses continue on every line
+            """
+            addresses = str(addresses).zfill(self.available_ports)
+            if ind == 0:
+                seq_text = seq_text +f"start: 0b111{addresses}, {duration} ns\n"
+            elif(ind > 0 and ind < seq_length):
+                seq_text = seq_text +f"       0b111{addresses}, {duration} ns\n"
+            else:
+                seq_text = seq_text +f"       0b111{addresses}, {duration} ns, branch, start"
+            
+            return seq_text
 
 
-    pbi = SpbiclPulseBlaster(None)
-    print(pbi.start())
-    print(pbi.stop())
-    print(pbi.clear())
+        # Gets a linear time sequence from the sequence generation class
+        linear_time_devices, step_times_ns = sequence_class.linear_time_sequence()
+        seq_length = len(step_times_ns)-2 # Checks if the length of a sequence is at least 2 for iteration or 1 for not iteration
+        
+        # If we have multiple instructions 
+        if seq_length > 0:
+            sequence_text = ""
+            for ind,step_time in enumerate(step_times_ns[:-1]):
+                addresses = 0
+                for device in sequence_class._devices:
+                    if step_time in linear_time_devices[device.address]["on_times_ns"]:
+                            addresses = addresses + pow(10,device.address)
+                
+                # Finds the duration by simple subtraction 
+                duration_ns = int(step_times_ns[ind+1]-step_time)
+                
+                # If the duration is longer than the maximum time for a single step it loops adding steps and subtracting the duration 
+                while duration_ns > self.maximum_step_time_s/seconds.ns.value:
+                    duration_ns = int(duration_ns - self.maximum_step_time_s/seconds.ns.value)
+                    # Adding 1 to sequence length so we don't have issues when a value is maxed out on time and is the end of the sequence
+                    sequence_text = adding_lines(ind,addresses,int(self.maximum_step_time_s/seconds.ns.value),seq_length+1,sequence_text)
+            
+                # Adding the next line to the sequence text and the remainder if there is any from looping 
+                sequence_text = adding_lines(ind,addresses,duration_ns,seq_length,sequence_text)
+        
+        # If there is only one instruction
+        elif seq_length == 0:
+            # With only one instruction there is no state changes so duration can be the maximum 
+            addresses = 0
+            for device_address in linear_time_devices:
+                    addresses = addresses + pow(10,device_address)
 
+            sequence_text = ""
+            # Adds a starting and looping line breaking one instruction into two 
+            sequence_text = adding_lines(0,addresses,int(self.maximum_step_time_s/seconds.ns.value),1,sequence_text)
+            sequence_text = adding_lines(1,addresses,int(self.maximum_step_time_s/seconds.ns.value),1,sequence_text)
+        else:
+            raise ValueError("Sequence must contain one or more steps")
 
+        return sequence_text
 
-
-
+# # # # # ##############################################################################################################
+# # # # # #     _    _
+# # # # # #    (o)--(o)
+# # # # # #   /.______.\
+# # # # # #   \________/
+# # # # # #  ./        \.
+# # # # # # ( .        , )
+# # # # # #  \ \_\\//_/ /
+# # # # # #   ~~  ~~  ~~ The frog of linear timing 
+# # # # # ##############################################################################################################
--- NV_ABJ/sequence_generation/sequence_generation.py
+++ NV_ABJ/sequence_generation/sequence_generation.py
@@ -3,7 +3,7 @@ __all__ = ["SequenceDevice","SequenceSubset","Sequence"]
 from dataclasses import dataclass
 from NV_ABJ import seconds
 
-@dataclass
+@dataclass(frozen=True)
 class SequenceDevice:
     """This is the class that determines the basic properties of a devices passed to a sequence 
 
@@ -18,10 +18,14 @@ class SequenceDevice:
     delayed_to_on_s:float = 0 # How many seconds it takes to turn on 
     device_status:bool = False # False indicates an off device when updating the devices True will be on
 
+    # This allows us to sort the devices based on the delay time 
+    def __lt__(self,other):
+        return self.delayed_to_on_s < other.delayed_to_on_s
+
 
 class SequenceSubset:
     
-    def __init__(self,steps:list=None,loop_steps=0):
+    def __init__(self,steps:list=None,loop_steps=0,devices:set = None):
         """This subset allows for a sequence to contain loops if you have a repeating sequence inside the overall sequence
         that is a subset and can be made with this class. This would be like an XY8 sequence 
 
@@ -34,6 +38,11 @@ class SequenceSubset:
         else:
             self.steps = steps
         self.loop_steps = loop_steps # when set to zero it wont be looped
+
+        if devices == None:
+            self._devices = set()
+        else:
+            self._devices = devices
 
     def __repr__(self):
         response = f"Following is looped {self.loop_steps} times\n"
@@ -57,24 +66,32 @@ class SequenceSubset:
         """
         step = {"devices":devices, "duration":duration, "time_unit":time_unit}
         self.steps.append(step)
+        for device in devices:
+            self._devices.add(device)
     
 
 
 class Sequence:
 
-    def __init__(self,sequence_subsets:list=None):
+    def __init__(self,sequence_subsets:list=None,devices:set = None):
         if sequence_subsets == None: 
             self.sequence_subsets = []
         else:
             self.sequence_subsets = sequence_subsets
+        if devices == None:
+            self._devices = set()
+        else:
+            self._devices = devices
             
     def add_step(self,devices:list=None,duration:float=None, time_unit:seconds=seconds.s):
         sub_sequence = SequenceSubset()
         sub_sequence.add_step(devices,duration,time_unit)
         self.sequence_subsets.append(sub_sequence)
+        self._devices.update(sub_sequence._devices)
     
     def add_sub_sequence(self, sub_sequence:SequenceSubset):
         self.sequence_subsets.append(sub_sequence)
+        self._devices.update(sub_sequence._devices)
 
     def __repr__(self):
         response = ""
@@ -82,87 +99,90 @@ class Sequence:
             subs = repr(subset)
             response = response + subs
         return response
+    
+    def linear_time_sequence(self)-> tuple[dict,set]:
+        """This function generates a linear time progression of the sequences and raises errors if a sequence is not possible due to timing of the 
+        delays on a device interacting with times it was previously on
+
+        Raises:
+            ValueError: if there is a time where a device is off for less than it's delay to on it will raise an error 
+
+        Returns:
+            tuple[dict,set]: returns a dictionary containing the devices and times each device is on and a set of all the state changes for a device 
+        """
+        # seq.add_sub_sequence(sub)
+        time_ns = 0 # Keeping track of the nominal time 
+        step_times_ns = set() # Set of all unique times 
+
+        devices_currently_on = set() # Keeping track of if devices are on(True) or off(False)
+        linear_time_devices = {} # A list of the devices broken into linear time 
+
+        # Creating a dictionary item for each device
+        for device in self._devices:
+            linear_time_devices[device.address] = {"device":device, "on_times_ns":set()}
+
+        # Opens each subset
+        for subset in self.sequence_subsets:
+            # Iterates over the loop repeatably 
+            for loop_iteration in range(subset.loop_steps+1):
+                # Goes through each step 
+                for step in subset.steps:
+
+                    # Goes through all devices 
+                    devices_on = step["devices"]
+                    devices_off = seq._devices - set(step["devices"])
+
+                    # Removing any devices that are no longer in the on state
+                    devices_currently_on = devices_currently_on-devices_off
+
+                    # Going down in delay time for devices that were off but are now on the last element is then all devices are on
+                    delays_decreasing = sorted(list(set(devices_on)-devices_currently_on), reverse=True)
+
+                    if delays_decreasing != []:
+                        for dev in delays_decreasing[:-1]:
+                            # If the device was off and has a delay we can account for it here 
+                            device_on_ns = int(time_ns - dev.delayed_to_on_s/seconds.ns.value)
+
+                            # Confirming there is no time where you have to trigger while it was on but requires a gap
+                            if len(list(linear_time_devices[dev.address]["on_times_ns"])) != 0:
+                                if max(linear_time_devices[dev.address]["on_times_ns"]) > device_on_ns:
+                                    raise ValueError(f"The device is turned off for less time then the delayed time {dev}")
+                                
+                            # Adjusting the time that the device may be turned on during 
+                            step_times_ns.add(device_on_ns) # Adding to the set incase there is a unique time 
+                            # Adds the needed start time for the device to be on if it takes time to turn on
+                            linear_time_devices[dev.address]["on_times_ns"].add(device_on_ns) 
+
+                        
+                        # Using the time for the last delay that had to be turned on for all devices on
+                        device_on_ns = int(time_ns - delays_decreasing[-1].delayed_to_on_s/seconds.ns.value)
+                        for dev in devices_on:
+                            linear_time_devices[dev.address]["on_times_ns"].add(device_on_ns)
+                            # Adds the device to those that are on 
+                            devices_currently_on.add(dev)
+                            step_times_ns.add(device_on_ns) # Adding to the set incase there is a unique time 
+
+                            # Confirming there is no time where you have to trigger while it was on but requires a gap
+                            if len(list(linear_time_devices[dev.address]["on_times_ns"])) != 0:
+                                if max(linear_time_devices[dev.address]["on_times_ns"]) > device_on_ns:
+                                    raise ValueError(f"The device is turned off for less time then the delayed time {dev}")
+
+                    else:
+                        for dev in devices_on:
+                            linear_time_devices[dev.address]["on_times_ns"].add(time_ns)
+                            devices_currently_on.add(dev)
+                            step_times_ns.add(time_ns) # Adding to the set incase there is a unique time 
+
+                    if devices_off == self._devices:
+                        step_times_ns.add(time_ns) # Adding to the set incase there is a unique time 
 
 
-dev1 = SequenceDevice(0,"device 1", delayed_to_on_s=100*seconds.ns.value)
-dev2 = SequenceDevice(1, "device 2", delayed_to_on_s=300*seconds.ns.value)
-dev3 = SequenceDevice(2, "device 3")
+                    # Increasing time by one step in the sequence 
+                    time_ns = int(time_ns+step["duration"]*step["time_unit"].value/seconds.ns.value)
 
-# sub = SequenceSubset()
-# sub.add_step([dev1,dev2,dev3],1, seconds.ns)
-# sub.add_step([dev2,dev3],20e-9)
-# sub.add_step([dev1,dev3],30e-9)
-# sub.loop_steps = 10
-# print(sub)
+        # Adding the ending time and converting set to a sorted list
+        step_times_ns.add(time_ns) 
+        step_times_ns = list(step_times_ns)
+        step_times_ns.sort()
 
-seq = Sequence()
-seq.add_step([dev1,dev2,dev3],10, time_unit = seconds.ns)
-seq.add_step([dev1,dev3],20, time_unit = seconds.ns)
-seq.add_step([dev1,dev2],30, time_unit = seconds.ns)
-
-# seq.add_sub_sequence(sub)
-print(seq)
-
-linear_time_devices = {} # A list of the devices broken into linear time 
-time_ns = 0 # Keeping track of the nominal time 
-
-device_states = {} # Keeping track of if devices are on(True) or off(False)
-
-# Opens each subset
-for subset in seq.sequence_subsets:
-    # Iterates over the loop repeatably 
-    for loop_iteration in range(subset.loop_steps+1):
-        # Goes through each step 
-        for step in subset.steps:
-            # Goes through all devices 
-            devices_in_step = []
-            for dev in step["devices"]:
-                # Adjusting the time that the device may be turned on during 
-                device_on_ns = time_ns - dev.delayed_to_on_s/seconds.ns.value
-
-                # Appending devices that are in a step to keep track of what devices are on 
-                devices_in_step.append(device_states)
-
-                if dev.address in linear_time_devices.keys():
-                    # If the device is off we want to turn it on 
-                    if not device_states[dev.address]:
-                        linear_time_devices[dev.address]["time_turn_on_ns"].append(device_on_ns)
-                        device_states[dev.address] = True 
-                    # If the device is on and continues to be on we can ignore that case
-                else:
-                    # The first instance of a device being turned on create the dictionary 
-                    linear_time_devices[dev.address] = {"device":dev, "time_turn_on_ns":[device_on_ns], "time_turn_off_ns":[]}
-                    device_states[dev.address] = True # Sets device to be known on 
-            
-            # Turning off all devices that were not in the step
-            for dev_address in device_states:
-                if dev_address not in devices_in_step:
-                    device_states[dev_address] = False
-                    linear_time_devices[dev.address]["time_turn_off_ns"].append(time_ns)
-            # Adjusting the time taken 
-            time_ns = time_ns + step["duration"]*step["time_unit"].value/seconds.ns.value
-
-print(linear_time_devices)
-
-import matplotlib.pyplot as plt
-offset_between_devices = 2
-
-for device_number, device_address in enumerate(linear_time_devices.keys()):
-    offset = offset_between_devices*device_number
-    time_and_signal = []
-    for times_on in linear_time_devices[device_address]["time_turn_on_ns"]:
-        time_and_signal.append((times_on,offset+1))
-    for times_off in linear_time_devices[device_address]["time_turn_off_ns"]:
-        time_and_signal.append((times_off,offset))
-
-    plt.plot(time_and_signal,label=linear_time_devices[device_address]["device"].device_label)
-
-plt.legend()
-plt.show()    
-
-
-                
-                    
-
-                
-
+        return linear_time_devices, step_times_ns
