7dde142e648491688743784dd49b6017753d473c
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2025-04-04 14:23:52 -0700

    working on confocal scans

--- NV_ABJ/experimental_logic/confocal_scanning.py
+++ NV_ABJ/experimental_logic/confocal_scanning.py
@@ -1,114 +1,163 @@
-__all__ = ["ConfocalScanningDisplay","ConfocalScanningControls"]
-from NV_ABJ import ScannerSingleAxis,PhotonCounter,meters
+__all__ = ["ConfocalControls"]
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
-import matplotlib.font_manager as fm
-
+# basic Imports
 import numpy as np
+import matplotlib.pyplot as plt
 from numpy.typing import NDArray
+from NV_ABJ import ScannerSingleAxis, PhotonCounter
 
-class ConfocalScanningDisplay:
-    """A grouping of functions that are used to quickly plot outputs for the confocal images 
-    """
-    
-    def plot_image_scan(image_data,x_positions_m,y_positions_m,length_units:meters=meters.m,include_scale_bar=False,include_axis=True,bar_size:int = 10e-6):
-        x_positions_m = x_positions_m/length_units.value
-        y_positions_m = y_positions_m/length_units.value
-        bar_size = bar_size
-
-        fig, ax = plt.subplots()
-        ax.imshow(image_data,extent=[x_positions_m[0],x_positions_m[1],y_positions_m[0],y_positions_m[1]]) 
-
-        if include_scale_bar:
-            fontprops = fm.FontProperties(size=18)
-
-            scalebar = AnchoredSizeBar(ax.transData,
-                                    bar_size, f'{bar_size} {length_units.name}', 'lower center', 
-                                    pad=0.1,
-                                    color='white',
-                                    frameon=False,
-                                    size_vertical=5,
-                                    fontproperties=fontprops)
-
-            ax.add_artist(scalebar)
-        
-        if not include_axis:
-            ax.axis(False)
-
-        return fig
-
-
-
-class ConfocalScanningControls:
-
-    def __init__(self,positioner_x:ScannerSingleAxis,positioner_y:ScannerSingleAxis,positioner_z:ScannerSingleAxis,photon_counter:PhotonCounter):
-        self.positioner_x = positioner_x
-        self.positioner_y = positioner_y
-        self.positioner_z = positioner_z
+class ConfocalControls:
+    def __init__(self,scanner_x:ScannerSingleAxis,scanner_y:ScannerSingleAxis,scanner_z:ScannerSingleAxis,photon_counter:PhotonCounter,
+                 tracking_xy_span:float = 3e-6,tracking_z_span:float = 3e-6,tracking_dwell_time_s:float = 5e-3,tracking_xy_number_of_points:int = 10,tracking_z_number_of_points:int = 20):
+        self.scanner_x = scanner_x
+        self.scanner_y = scanner_y
+        self.scanner_z = scanner_z
         self.photon_counter = photon_counter
-    
-    def two_dimensional_scanning_raw(self,positions_x_m:tuple,positions_y_m:tuple,dwell_time_s:float,resolution:int)-> tuple[NDArray[np.int64],NDArray[np.float64],NDArray[np.float64]]:
-        """This creates a two dimensional array over a selected region to scan 
+        
+        # Tracking parameters
+        self.tracking_xy_span = tracking_xy_span
+        self.tracking_z_span = tracking_z_span
+        self.tracking_dwell_time_s = tracking_dwell_time_s
+        self.tracking_xy_number_of_points = tracking_xy_number_of_points
+        self.tracking_z_number_of_points = tracking_z_number_of_points
+
+
+
+
+
+    def xy_scan(self,dwell_time_s:float,x_positions:NDArray,y_positions:NDArray,z_position:float)-> tuple[NDArray,NDArray,NDArray]:
+        """An xy scan has the same z height for all points and translates to the x and y positions. This instance of the xy 
+        scan iterates between scanning forward and backward so there is no sudden movement to the confocal. The arrays from 
+        x and y when added will be sorted to ensure the locations are sequential. 
 
         Args:
-            positions_x_m (tuple): Scanning ranges for x 
-            positions_y_m (tuple): Scanning ranges for y
-            dwell_time_s (float): How long we will dwell at each point
-            resolution (int): How many points per axis will be taken 
+            dwell_time_s (float): How long we dwell at each point before moving to the next point
+            x_positions (NDArray): An array of the x_positions you want to scan over 
+            y_positions (NDArray): An array of the y_positions you want to scan over 
+            z_position (float): The z position we want to scan at
 
         Returns:
-            tuple[NDArray[np.int64],NDArray[np.float64],NDArray[np.float64]]: (counts,x_positions,y_positions) Returns the counts for the photons along with the nominal x and y positions of the points 
+            NDArray: Photon counts. A 2D array of the photons per second at each point in the x y positions 
+            NDArray: X positions. A sorted x array of the positions passed originally to the function 
+            NDArray: Y positions. A sorted y array of the positions passed originally to the function
         """
-        # Make positions space
-        x_positions = np.linspace(positions_x_m[0],positions_x_m[1],resolution)
-        y_positions = np.linspace(positions_y_m[0],positions_y_m[1],resolution)
 
-        # Preallocate memory 
-        photon_count_image = np.zeros((resolution,resolution))
+        # Making sure the lists are ordered correctly 
+        x_positions = sorted(x_positions)
+        y_positions = sorted(y_positions)
 
-        # Iterate through all positions 
-        for ind_y,y in enumerate(y_positions):
-            for ind_x,x in enumerate(x_positions):
-                self.positioner_x.set_position_m(x)
-                self.positioner_y.set_position_m(y)
+        # Getting the basic length which is repeatedly used
+        x_length = len(x_positions)
 
-                # Get photon counts and return the matrix 
-                photon_count = self.photon_counter.get_counts_raw(dwell_time_s)
-                photon_count_image[ind_x,ind_y] = photon_count
-        
-        return photon_count_image,x_positions,y_positions
+        # Pre allocating matrices
+        xy_counts = np.zeros((x_length,len(y_positions)))
+        line_counts = np.zeros(x_length)
+
+        # Reversed list to go backward with every other iteration
+        reversed_x = x_positions[::-1]
     
-    def two_dimensional_scanning_per_second(self,positions_x_m:tuple,positions_y_m:tuple,dwell_time_s:float,resolution:int)-> tuple[NDArray[np.int64],NDArray[np.float64],NDArray[np.float64]]:
-        counts,x,y  = self.two_dimensional_scanning_raw(positions_x_m,positions_y_m,dwell_time_s,resolution)
-        counts_per_second = np.round(counts/dwell_time_s)
-        return counts_per_second,x,y
+        # Getting original z to return to 
+        z_original = self.scanner_z._position_m
+        
+        # Opening all scanners and photon counters
+        with self.scanner_x as x_con, self.scanner_y as y_con, self.scanner_z as z_con, self.photon_counter as pc:
+            # Sets the initial position to be location at the place that the user requested
+            z_con.set_position_m(z_position)
 
-    def z_scan_raw(self,z_positions_m:tuple,dwell_time_s:float,resolution:int)->tuple[NDArray[np.int64],NDArray[np.float64]]:
-        """Scans at a single x and y position and scans in z
+            # Iterates through y setting the position once per line 
+            for ind_y,y_loc in enumerate(y_positions):
+                y_con.set_position_m(y_loc)
+
+                # Flips the even and odd rows so we don't have jumps going back and forth on the x axis 
+                if ind_y%2 == 0:
+                    for ind_x,x_loc in enumerate(x_positions):
+                        x_con.set_position_m(x_loc)
+                        counts = pc.get_counts_per_second(dwell_time_s=dwell_time_s)
+                        line_counts[ind_x] = counts
+
+                else:
+                    for ind_x,x_loc in enumerate(reversed_x):
+                        x_con.set_position_m(x_loc)
+                        counts = pc.get_counts_per_second(dwell_time_s=dwell_time_s)
+                        line_counts[(x_length-1)-ind_x] = counts
+
+                # Adds a full line at a time 
+                xy_counts[:,ind_y] = line_counts
+
+            
+            # Resetting back to original z position
+            z_con.set_position_m(z_original)
+
+        # xy_counts = np.rot90(xy_counts)
+        # xy_counts = xy_counts.transpose()
+
+        return xy_counts,np.array(x_positions),np.array(y_positions)
+    
+    def z_scan(self,dwell_time_s:float,x_position:float,y_position:float, z_positions:NDArray)->tuple[NDArray,NDArray]:
+        """This is a z scan over a stationary xy position. It then goes through the z positions sequential
+         after ordering the lists to be in the correct orientation
 
         Args:
-            z_positions_m (tuple): (z minimum, z maximum)
-            dwell_time_s (float): time for taking data before moving to next point
-            resolution (int): how many points of data will be taken 
+            dwell_time_s (float): How long we dwell at each point before moving to the next point
+            x_position (float): An array of the x position you want to scan at 
+            y_position (float): An array of the y position you want to scan at 
+            z_positions (NDArray): An array of the z positions you want to scan over 
 
         Returns:
-            tuple[NDArray[np.int64],NDArray[np.float64]]: _description_
+            NDArray: Photon counts. An array of the photons per second at each point in the z positions
+            NDArray: Z positions. A sorted array for the z positions 
         """
-        z_positions = np.linspace(z_positions_m[0],z_positions_m[1],resolution)
-        photon_counts = np.zeros(resolution)
+        # Sorting z list
+        z_positions = sorted(z_positions)
 
-        for ind,z in enumerate(z_positions):
-            self.positioner_z.set_position_m(z)
-            photon_count = self.photon_counter.get_counts_raw(dwell_time_s)
-            photon_counts[ind] = photon_count
+        # Preallocating z counts
+        photon_counts = np.zeros(len(z_positions))
+
+        # Getting original z to return to 
+        z_original = self.scanner_z._position_m
         
-        return photon_counts, z_positions
+        # Opening all scanners and photon counters
+        with self.scanner_x as x_con, self.scanner_y as y_con, self.scanner_z as z_con, self.photon_counter as pc:
+            
+            # Setting the xy position of interest 
+            x_con.set_position_m(x_position)
+            y_con.set_position_m(y_position)
 
-    def z_scan_per_second(self,z_positions_m:tuple,dwell_time_s:float,resolution:int)->tuple[NDArray[np.int64],NDArray[np.float64]]:
-        counts, z = self.z_scan_raw(z_positions_m,dwell_time_s,resolution)
-        counts_per_second = np.round(counts/dwell_time_s)
-        return counts_per_second,z
+            # Iterating over all desired z positions 
+            for ind_z,z_loc in enumerate(z_positions):
+                z_con.set_position_m(z_loc)
+                photon_counts[ind_z] = pc.get_counts_raw(dwell_time_s)
 
-    def tracking_on_point(self):
-        ...
+            # Resetting back to original z position
+            z_con.set_position_m(z_original)
+        
+        return photon_counts,np.array(z_positions)
+    
+    def tracking(self,x_position:float,y_position:float,z_position:float)->tuple[float,float,float]:
+        xy_span = self.tracking_xy_span
+        z_span = self.tracking_z_span
+        dwell_time_s = self.tracking_dwell_time_s
+        xy_number_of_points = self.tracking_xy_number_of_points
+        z_number_of_points = self.tracking_z_number_of_points
+
+        x_positions = np.linspace(x_position-xy_span/2,x_position+xy_span/2,xy_number_of_points)
+        y_positions = np.linspace(y_position-xy_span/2,y_position+xy_span/2,xy_number_of_points)
+        z_positions = np.linspace(z_position-z_span/2,z_position+z_span/2,z_number_of_points)
+
+
+        xy_2d_scan, _ , _ = self.xy_scan(dwell_time_s,x_positions,y_positions,z_position)
+
+        flat_index = np.argmax(xy_2d_scan)
+
+        row_index, col_index = np.unravel_index(flat_index, xy_2d_scan.shape)
+
+        x_pos = x_positions[row_index]
+        y_pos = y_positions[col_index]
+
+        z_1d_scan, _ = self.z_scan(dwell_time_s,x_pos,y_pos,z_positions)
+
+        z_pos = z_positions[np.argmax(z_1d_scan)]
+
+        return x_pos,y_pos,z_pos
+    
+
